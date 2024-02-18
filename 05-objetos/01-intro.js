b41a1029d0a5b1ce2a1b8f56a309f3191a9a2578
DeibyAA <deibyarroyave4@gmail.com>
2024-02-17 20:38:44 -0500

    intro

---
+++ 05-objetos/01-intro.js
@@ -0,0 +1,13 @@
+let user = {
+    email: 'cutrepez81@gmail.com',
+    name: 'Deiby',
+    direccion: {
+        calle: '3000 viviendas',
+        numero: 10,
+    },
+    activo: true,
+    recuperarClave: function () {
+        console.log('recuperando clave...');
+    },
+};
+
